0838362ea3f4677a84678822e778f8d370a19218
Ericson-Manalo <ericmana98@gmail.com>
2023-07-19 18:52:02 +0200

    used the function config to call from my db.php the array

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
 
-    // $cards = config('db.')
+    $cards = config('db.cards');
 
     $navbarLinks = config('db.navbarLinks');
-    return view('home', compact('navbarLinks'));
+    return view('home', compact('cards', 'navbarLinks'));
 });
