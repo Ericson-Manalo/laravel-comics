ab6542bb9ac0b76f0f3fbb7ab0eda5e493ec730a
Ericson-Manalo <ericmana98@gmail.com>
2023-07-19 19:05:45 +0200

    corrected the images by using function vite:asset

--- resources/views/partials/section.blade.php
+++ resources/views/partials/section.blade.php
@@ -1,31 +1,31 @@
 <section>
 <div class="main-section">
             <div class="item-container flex">
-                <img src="../assets/images/img/buy-comics-digital-comics.png" alt="Digital Comics">
+                <img src="{{Vite::asset ('resources/img/buy-comics-digital-comics.png')}}" alt="Digital Comics">
                 <p>
                     DIGITAL COMICS 
                 </p>
             </div>
             <div class="item-container flex">
-                <img src="../assets/images/img/buy-comics-merchandise.png" alt="DC Merchandise">
+                <img src="{{Vite::asset ('resources/img/buy-comics-merchandise.png')}}" alt="DC Merchandise">
                 <p>
                     DIGITAL COMICS 
                 </p>
             </div>
             <div class="item-container flex">
-                <img src="../assets/images/img/buy-comics-subscriptions.png" alt="Subscription">
+                <img src="{{Vite::asset ('resources/img/buy-comics-subscriptions.png')}}" alt="Subscription">
                 <p>
                     DIGITAL COMICS 
                 </p>
             </div>
             <div class="item-container flex">
-                <img src="../assets/images/img/buy-comics-shop-locator.png" alt="Comic Shop Locator">
+                <img src="{{Vite::asset ('resources/img/buy-comics-shop-locator.png')}}" alt="Comic Shop Locator">
                 <p>
                     DIGITAL COMICS 
                 </p>
             </div>
             <div class="item-container flex">
-                <img src="../assets/images/img/buy-dc-power-visa.svg" alt="DC Power Visa">
+                <img src="{{Vite::asset ('resources/img/buy-dc-power-visa.svg')}}" alt="DC Power Visa">
                 <p>
                     DIGITAL COMICS 
                 </p>
