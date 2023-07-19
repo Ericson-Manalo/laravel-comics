3ce657e1df1c679cabb671b4e23f681f797c2e80
Ericson-Manalo <ericmana98@gmail.com>
2023-07-19 19:22:21 +0200

    created file footer.blade.php

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,146 @@
+<footer>
+        <div class="upper-side">
+            <div class="upper-footer">
+                <div class="list-item">
+                    <div class="margin-right">
+                        <h3>
+                            DC COMICS
+                        </h3>
+                        <ul class="list">
+                            <li>
+                                Characters
+                            </li>
+                            <li>
+                                Comics
+                            </li>
+                            <li>
+                                Movies
+                            </li>
+                            <li>
+                                TV
+                            </li>
+                            <li>Games</li>
+                            <li>
+                                Videos
+                            </li>
+                            <li>
+                                News
+                            </li>
+                        </ul>
+
+                        <h3 class="margin-top">
+                            SHOP
+                        </h3>
+                        <ul class="list">
+                            <li>
+                                Shop DC
+                            </li>
+                            <li>
+                                Shop DC Collectibles
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="margin-right">
+                        <h3>
+                            DC
+                        </h3>
+                        <ul class="list">
+                            <li>
+                                Term Of Use
+                            </li>
+                            <li>
+                                Privacy policy (New)
+                            </li>
+                            <li>
+                                Ad Choices
+                            </li>
+                            <li>
+                                Advertising
+                            </li>
+                            <li>
+                                Jobs
+                            </li>
+                            <li>
+                                Subscriptions
+                            </li>
+                            <li>
+                                Talent Workshops
+                            </li>
+                            <li>
+                                CPSC Certificates
+                            </li>
+                            <li>
+                                Ratings
+                            </li>
+                            <li>
+                                Shop Help
+                            </li>
+                            <li>
+                                Contact Us
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div>
+                        <h3>
+                            SITES
+                        </h3>
+                        <ul class="list">
+                            <li>
+                                DC
+                            </li>
+                            <li>
+                                MAD Magazine
+                            </li>
+                            <li>
+                                DC Kids
+                            </li>
+                            <li>
+                                DC Universe
+                            </li>
+                            <li>
+                                DC Power Visa
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+        
+                <div class="logo-item">
+                    
+                </div>
+            </div>
+        </div>
+
+        <div class="lower-side">
+            <div class="lower-footer">
+                <div class="sign-up">
+                    SIGN-UP NOW!
+                </div>
+                <div class="social-side flex">
+                    <h2 class="follow">
+                        FOLLOW US
+                    </h2>
+                    <div class="social-icon">
+                        <a href="#">
+                            <img src="../assets/images/img/footer-facebook.png" alt="Facebook logo">
+                        </a>
+                        <a href="#">
+                            <img src="../assets/images/img/footer-twitter.png" alt="Facebook logo">
+                        </a>
+                        <a href="#">
+                            <img src="../assets/images/img/footer-youtube.png" alt="Facebook logo">
+                        </a>
+                        <a href="#">
+                            <img src="../assets/images/img/footer-pinterest.png" alt="Facebook logo">
+                        </a>
+                        <a href="#">
+                            <img src="../assets/images/img/footer-periscope.png" alt="Facebook logo">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </footer>
