8bcc6383054a256d54b0766b3ab18e405b84d90e
Ericson-Manalo <ericmana98@gmail.com>
2023-07-19 17:31:44 +0200

    imported the header of the app

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="header-section">
+        <img src="{{ Vite::asset ('resources/img/dc-logo.png') }}" alt="Dc Main Logo">
+        <nav class="nav-bar">
+            <ul>
+                <li>
+                    home
+                </li>
+            </ul>
+        </nav>
+    </div>
+</header>
