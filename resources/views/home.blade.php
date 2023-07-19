fddd0c148917e43f0eaba5d9c2323743aa093882
Ericson-Manalo <ericmana98@gmail.com>
2023-07-19 18:52:33 +0200

    made the structure of my main, in which i used the foreach

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,9 +10,20 @@
     <div class="main-content">
         <section class="main-section">
             <div class="card-container">
-                <SingleCardDc v-for="card in cardList"
-                                :cardElement="card"/>
+                <!-- <SingleCardDc v-for="card in cardList"
+                                :cardElement="card"/> -->
+                @foreach ($cards as $card)
+                    <article>
+                        <div class="card-container">
+                            <img src="{{$card['thumb']}}" alt="DC Cards" class="card">
+                            <h3>
+                                {{$card['title']}}
+                            </h3>
+                        </div>
+                    </article>
+                @endforeach
             </div>
+
         </section>
     </div>
 
