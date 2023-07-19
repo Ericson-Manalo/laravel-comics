713c2f8deb5109baa0ad0a7f4cacccf2eb8961b7
Ericson-Manalo <ericmana98@gmail.com>
2023-07-19 18:36:39 +0200

    imported the html content of the main into the home app

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,28 @@
 @extends('layouts.app')
 
 
-@section('title', 'Checking if it works!')
+@section('Dc Comics')
 
 
 @section('main-content')
-    @dd($navbarLinks)
+    <div class="jumbotron"> 
+    </div>
+    <div class="main-content">
+        <section class="main-section">
+            <div class="card-container">
+                <SingleCardDc v-for="card in cardList"
+                                :cardElement="card"/>
+            </div>
+        </section>
+    </div>
+
+    <div class="load-btn">
+        <p class="load-more">
+            LOAD MORE
+        </p>
+    </div>
+
+    <h2>
+        CURRENT SERIES
+    </h2>
 @endsection
